2c5128982c63397c45187460a1b18ed31c5efb54
UniqueIam <abhimanyukumar83097@gmail.com>
2025-06-12 09:05:44 +0530

    updated backend url

--- src/Utils/urlConfig.jsx
+++ src/Utils/urlConfig.jsx
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:8082';
+// export const BASE_URL = 'http://localhost:8082';
+export const BACKEND_URL = 'https://invo-sync-backend.vercel.app'
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -93,7 +93,7 @@ const Login = () => {
           </div>
         </form>
         <p className="text-center text-sm text-gray-600 mt-4">
-          Don’t have an account? <a href="/register" className="text-blue-600 hover:underline">Register</a>
+          Don’t have an account? <a href="/" className="text-blue-600 hover:underline">Register</a>
         </p>
       </div>
     </div>
