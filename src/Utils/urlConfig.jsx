b97910567e7017a95293c332dcc0664b3bbe31a9
UniqueIam <abhimanyukumar83097@gmail.com>
2025-06-12 09:14:45 +0530

    base url

--- src/Utils/urlConfig.jsx
+++ src/Utils/urlConfig.jsx
@@ -1,2 +1,1 @@
-// export const BASE_URL = 'http://localhost:8082';
-export const BACKEND_URL = 'https://invo-sync-backend.vercel.app'
+export const BASE_URL = 'https://invo-sync-backend.vercel.app';
